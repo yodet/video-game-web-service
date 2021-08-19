f3075cc70137b95386ab42b22cc692589d252285
yodet <82468898+yodet@users.noreply.github.com>
2021-08-18 21:43:32 -0700

    Create games-title.js

---
+++ data/games-title.js
@@ -0,0 +1,46 @@
+{
+	"title": "Video Games by Title",
+	"games":[
+			  {
+                "Title": "Counter-Strike Global Offensive",
+				"Genre":"Tactical Shooter",
+				"Company":"Valve",
+				"Year": 2012,
+				"Rating":"M",
+				"Image":"counter-strike-global-offensive.jpg"
+
+			       },
+			       {
+                "Title": "Minecraft",
+				"Genre":"Sandbox",
+				"Company":"Mojang",
+				"Year": 2011,
+				"Rating":"E",
+				"Image":"portal.jpg"
+              },
+              {
+                "Title": "Portal",
+        "Genre":"Puzzle",
+        "Company":"Valve",
+        "Year": 2007,
+        "Rating":"T",
+        "Image":"portal.jpg"
+              },
+              {
+                "Title": "Sonic the Hedgehog",
+				"Genre":"Platformer",
+				"Company":"Sega",
+				"Year": 1991,
+				"Rating":"E",
+				"Image":"sonic-the-hedgehog.jpg"
+              },
+              {
+			          "Title": "Super Mario Kart",
+				"Genre":"Racing",
+				"Company":"Nintendo",
+				"Year": 1992,
+				"Rating":"E10",
+				"Image":"super-mario-kart.jpg"
+			}
+    ]
+    }
