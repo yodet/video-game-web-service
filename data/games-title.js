2ce388d840d9460fae33ac413db995c57b7c3294
yodet <82468898+yodet@users.noreply.github.com>
2021-08-19 12:23:12 -0700

    Update games-title.js

--- data/games-title.js
+++ data/games-title.js
@@ -1,68 +1,54 @@
 {
-	"title": "Video Games by Title",
-	"games":[
-			  {
-                "Title": "Counter-Strike Global Offensive",
-				"Genre":"Tactical Shooter",
-				"Company":"Valve",
-				"Year": 2012,
-				"Rating":"M",
-				"Image":"counter-strike-global-offensive.jpg"
-
-<<<<<<< HEAD
-			  },
-			  {
-=======
-			       },
-			       {
->>>>>>> 6968c36bb37bacc6f0e9dfabed5ffd817024baae
-                "Title": "Minecraft",
-				"Genre":"Sandbox",
-				"Company":"Mojang",
-				"Year": 2011,
-				"Rating":"E",
-				"Image":"minecraft.jpg"
-              },
-              {
-                "Title": "Portal",
-<<<<<<< HEAD
-                    "Genre":"Puzzle",
-                    "Company":"Valve",
-                    "Year": 2007,
-                    "Rating":"T",
-                    "Image":"portal.jpg"
-=======
+"title": "Video Games by Title",
+"games":[
+	{
+        "Title": "Counter-Strike Global Offensive",
+	"Genre":"Tactical Shooter",
+	"Company":"Valve",
+	"Year": 2012,
+	"Rating":"M",
+	"Image":"counter-strike-global-offensive.jpg"
+	},
+	{
+        "Title": "Minecraft",
+	"Genre":"Sandbox",
+	"Company":"Mojang",
+	"Year": 2011,
+	"Rating":"E",
+	"Image":"minecraft.jpg"
+        },
+        {
+        "Title": "Portal",
         "Genre":"Puzzle",
         "Company":"Valve",
         "Year": 2007,
         "Rating":"T",
         "Image":"portal.jpg"
->>>>>>> 6968c36bb37bacc6f0e9dfabed5ffd817024baae
-              },
-              {
-                "Title": "Sonic the Hedgehog",
-				"Genre":"Platformer",
-				"Company":"Sega",
-				"Year": 1991,
-				"Rating":"E",
-				"Image":"sonic-the-hedgehog.jpg"
-              },
-              {
-<<<<<<< HEAD
-			  "Title": "Super Mario Kart",
-=======
-			          "Title": "Super Mario Kart",
->>>>>>> 6968c36bb37bacc6f0e9dfabed5ffd817024baae
-				"Genre":"Racing",
-				"Company":"Nintendo",
-				"Year": 1992,
-				"Rating":"E10",
-				"Image":"super-mario-kart.jpg"
-			}
-<<<<<<< HEAD
-        ]
-    }
-=======
+        },
+        {
+        "Genre":"Puzzle",
+        "Company":"Valve",
+        "Year": 2007,
+        "Rating":"T",
+        "Image":"portal.jpg"
+        },
+        {
+        "Title": "Sonic the Hedgehog",
+	"Genre":"Platformer",
+	"Company":"Sega",
+	"Year": 1991,
+	"Rating":"E",
+	"Image":"sonic-the-hedgehog.jpg"
+        },
+        {
+        "Title": "Super Mario Kart",
+	"Genre":"Racing",
+	"Company":"Nintendo",
+	"Year": 1992,
+	"Rating":"E10",
+	"Image":"super-mario-kart.jpg"
+	}   
     ]
-    }
->>>>>>> 6968c36bb37bacc6f0e9dfabed5ffd817024baae
+
+}
+
